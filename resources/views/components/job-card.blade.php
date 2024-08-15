cf5548573971b3b5a6f108685771def2e2edd01b
HurasAdam <huradam@wp.pl>
2024-08-15 21:19:19 +0200

    Intial commit: Added Basic Layout and ui components

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,19 @@
+<div class="p-4 bg-white/5 rounded flex flex-col text-center">
+    <div class="self-start text-sm">Laravel</div>
+    <div class="py-8 font-bold">
+        <x-section-header>PHP Backend Developer</x-section-header>
+        <p>75.000 Full Time</p>
+
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+
+        <div>
+            <x-tag>PHP</x-tag>
+            <x-tag>BACKEND</x-tag>
+
+
+
+        </div>
+        <img src="http://placehold.it/42/42" alt="author-image" class="rounded-xl">
+    </div>
+</div>
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Job Market</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-black text-white">
+
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4 border-b border-white/10">
+            <div>
+                <a href="/">
+                    <img class="w-10 h-auto" src="{{ Vite::asset('resources\images\logo_4.svg') }}" alt="">
+                </a>
+            </div>
+            <div class="space-x-9 font-bold">
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
+            <div>
+                <a href="#">Post a job</a>
+            </div>
+        </nav>
+        <main class="mt-10 max-w-[968px] mx-auto">
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+
+</html>
