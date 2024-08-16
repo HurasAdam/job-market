4a1fb30fca0db8f1d21ca9a65529550e075692a9
HurasAdam <huradam@wp.pl>
2024-08-16 21:39:21 +0200

    Feat: Added Job and Employer : Controller, Policy, Factory, migrations.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,13 @@
 <x-layout>
-    <div class="space-y-10">
+    <div class="space-y-10 pt-6">
         <section class="text-center">
             <h1 class="font-bold text-4xl">Find your dream job</h1>
             <form action="" class="mt-6">
                 <input type="text" placeholder="Project Manager"
-                    class="rounded bg-white/15 border-white/10 px-5 py-4 w-full max-w-xl ">
+                    class="rounded bg-white/15 border-white/5 px-5 py-4 w-full max-w-xl ">
             </form>
         </section>
-        <section>
+        <section class="pt-10">
             <x-section-header>Top jobs</x-section-header>
             <div class="grid lg:grid-cols-3 gap-8 mt-6">
                 <x-job-card></x-job-card>
