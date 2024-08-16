454b95bb797ebbd088c6ba54d6ef4963e836bdf0
HurasAdam <huradam@wp.pl>
2024-08-16 21:50:52 +0200

    Feat: Added relationship between Job, Employer and User models.

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Models\Employer;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
@@ -43,5 +44,8 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function employer(){
+return $this->hasOne(Employer::class);
     }
 }
